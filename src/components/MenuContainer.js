f6c075017daf12beb37dc881b937a19ab2dffb06
Gustav <gustav@kramnas.se>
2022-12-28 17:08:30 +0100

    style: menu styling css
    Co-authored-by: Kristallers <Kristallers@users.noreply.github.com>

--- src/components/MenuContainer.js
+++ src/components/MenuContainer.js
@@ -1,71 +1,76 @@
 import noodleImage from "./seafood-noodles.svg";
+
+import "../styles/menu-container.css";
+
 import MenuItem from "./MenuItem";
 
 const MenuContainer = () => {
-	const menuArray = [
-		{
-			id: 1,
-			title: "Sea noodles",
-			price: 15,
-			image: noodleImage,
-			description: "good",
-			category: "soup",
-		},
-		{
-			id: 2,
-			title: "Beef noodles",
-			price: 20,
-			image: noodleImage,
-			description: "very good",
-			category: "warm",
-		},
-		{
-			id: 3,
-			title: "Shrimp noodles",
-			price: 15,
-			image: noodleImage,
-			description: "nice",
-			category: "",
-		},
-		{
-			id: 1,
-			title: "Sea noodles",
-			price: 15,
-			image: noodleImage,
-			description: "good",
-			category: "soup",
-		},
-		{
-			id: 2,
-			title: "Beef noodles",
-			price: 20,
-			image: noodleImage,
-			description: "very good",
-			category: "warm",
-		},
-		{
-			id: 3,
-			title: "Shrimp noodles",
-			price: 15,
-			image: noodleImage,
-			description: "nice",
-			category: "",
-		},
-	];
+  const menuArray = [
+    {
+      id: 1,
+      title: "Sea noodles",
+      price: 15,
+      image: noodleImage,
+      description: "good",
+      category: "soup",
+    },
+    {
+      id: 2,
+      title: "Beef noodles",
+      price: 20,
+      image: noodleImage,
+      description: "very good",
+      category: "warm",
+    },
+    {
+      id: 3,
+      title: "Shrimp noodles",
+      price: 15,
+      image: noodleImage,
+      description: "nice",
+      category: "",
+    },
+    {
+      id: 1,
+      title: "Sea noodles",
+      price: 15,
+      image: noodleImage,
+      description: "good",
+      category: "soup",
+    },
+    {
+      id: 2,
+      title: "Beef noodles",
+      price: 20,
+      image: noodleImage,
+      description: "very good",
+      category: "warm",
+    },
+    {
+      id: 3,
+      title: "Shrimp noodles",
+      price: 15,
+      image: noodleImage,
+      description: "nice",
+      category: "",
+    },
+  ];
 
-	return (
-		<div>
-			<h2>Choose dishes</h2>
-			{menuArray.map((item) => (
-				<MenuItem
-					imgSrc={item.image}
-					title={item.title}
-					description={item.description}
-					price={item.price}
-				/>
-			))}
-		</div>
-	);
+  return (
+    <div className="menu-wrapper">
+      <h2 className="h2">Choose dishes</h2>
+      <div className="menu-items-wrapper">
+        {menuArray.map((item) => (
+          <MenuItem
+            imgSrc={item.image}
+            title={item.title}
+            description={item.description}
+            price={item.price}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MenuContainer;
--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,12 +1,14 @@
+import "../styles/menu-item.css";
+
 const MenuItem = (props) => {
-	return (
-		<div>
-			<img src={props.imgSrc} />
-			<h3>{props.title}</h3>
-			<p>{props.description}</p>
-			<p>{props.price}€</p>
-		</div>
-	);
+  return (
+    <div className="item-container">
+      <img className="food-icon" src={props.imgSrc} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+      <p>{props.price}€</p>
+    </div>
+  );
 };
 
 export default MenuItem;
