4a74d6ae9aca0d04c0ff397bf48a99b4b0da05bd
Gustav <gustav@kramnas.se>
2022-12-29 22:05:55 +0100

    style: css for the menu continaer and menu item

--- src/components/MenuContainer.js
+++ src/components/MenuContainer.js
@@ -8,64 +8,70 @@ const MenuContainer = () => {
   const menuArray = [
     {
       id: 1,
-      title: "Sea noodles",
-      price: 15,
+      title: "Spicy seasoned seafood noodles",
+      price: 13.8,
       image: noodleImage,
-      description: "good",
+      description: "amazing",
       category: "soup",
+      availability: 16 + " bowls available",
     },
     {
       id: 2,
-      title: "Beef noodles",
-      price: 20,
+      title: "Spicy instant noodle with special omelette",
+      price: 22.5,
       image: noodleImage,
       description: "very good",
       category: "warm",
+      availability: 13 + " bowls available",
     },
     {
       id: 3,
-      title: "Shrimp noodles",
-      price: 15,
+      title: "Spicy seasoned seafood noodles",
+      price: 16.3,
       image: noodleImage,
       description: "nice",
       category: "",
+      availability: 28 + " bowls available",
     },
     {
       id: 1,
-      title: "Sea noodles",
-      price: 15,
+      title: "Spicy seasoned seafood noodles",
+      price: 15.5,
       image: noodleImage,
       description: "good",
       category: "soup",
+      availability: 9 + " bowls available",
     },
     {
       id: 2,
-      title: "Beef noodles",
-      price: 20,
+      title: "Spicy seasoned seafood noodles",
+      price: 19.9,
       image: noodleImage,
       description: "very good",
       category: "warm",
+      availability: 5 + " bowls available",
     },
     {
       id: 3,
-      title: "Shrimp noodles",
-      price: 15,
+      title: "Spicy seasoned seafood noodles",
+      price: 9.9,
       image: noodleImage,
       description: "nice",
       category: "",
+      availability: 20 + " bowls available",
     },
   ];
 
   return (
     <div className="menu-wrapper">
-      <h2 className="h2">Choose dishes</h2>
+      <h2 className="h2">Choose Dishes</h2>
       <div className="menu-items-wrapper">
         {menuArray.map((item) => (
           <MenuItem
             imgSrc={item.image}
             title={item.title}
-            description={item.description}
             price={item.price}
+            availability={item.availability}
           />
         ))}
       </div>
--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -3,10 +3,14 @@ import "../styles/menu-item.css";
 const MenuItem = (props) => {
   return (
     <div className="item-container">
-      <img className="food-icon" src={props.imgSrc} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-      <p>{props.price}€</p>
+      <img className="food-icon" src={props.imgSrc} alt="food-icon" />
+      <div className="text-wrapper">
+        <p className="food-title">{props.title}</p>
+        <div className="food-price-availability-wrapper">
+          <p className="food-price">$ {props.price}</p>
+          <p className="food-availability">{props.availability}</p>
+        </div>
+      </div>
     </div>
   );
 };
