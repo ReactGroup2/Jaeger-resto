b6a2fb664486be804e6f6cbef53ee6878f17ae36
Ester Cortes <002escor@gmail.com>
2022-12-29 14:21:53 +0100

    dummy data

--- src/components/MenuContainer.js
+++ src/components/MenuContainer.js
@@ -1,62 +1,11 @@
-import noodleImage from "./seafood-noodles.svg";
+import data from "../data/data.js";
 import MenuItem from "./MenuItem";
 
 const MenuContainer = () => {
-	const menuArray = [
-		{
-			id: 1,
-			title: "Sea noodles",
-			price: 15,
-			image: noodleImage,
-			description: "good",
-			category: "soup",
-		},
-		{
-			id: 2,
-			title: "Beef noodles",
-			price: 20,
-			image: noodleImage,
-			description: "very good",
-			category: "warm",
-		},
-		{
-			id: 3,
-			title: "Shrimp noodles",
-			price: 15,
-			image: noodleImage,
-			description: "nice",
-			category: "",
-		},
-		{
-			id: 1,
-			title: "Sea noodles",
-			price: 15,
-			image: noodleImage,
-			description: "good",
-			category: "soup",
-		},
-		{
-			id: 2,
-			title: "Beef noodles",
-			price: 20,
-			image: noodleImage,
-			description: "very good",
-			category: "warm",
-		},
-		{
-			id: 3,
-			title: "Shrimp noodles",
-			price: 15,
-			image: noodleImage,
-			description: "nice",
-			category: "",
-		},
-	];
-
 	return (
 		<div>
 			<h2>Choose dishes</h2>
-			{menuArray.map((item) => (
+			{data.map((item) => (
 				<MenuItem
 					imgSrc={item.image}
 					title={item.title}
