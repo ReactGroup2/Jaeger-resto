bacab012708b7f113ada720a12685baa06347215
Ester Cortes <002escor@gmail.com>
2023-01-02 10:48:05 +0100

    style: fixed spelling (all -> All)

--- src/Components/Header/Navbar.js
+++ src/Components/Header/Navbar.js
@@ -4,32 +4,32 @@ import { MenuContext } from "../data/MenuContext";
 import Data from "../data/Data";
 
 const Navbar = () => {
-  const [menu,setMenu] = useContext(MenuContext);
-  const [data] = useState(Data);
+	const [menu, setMenu] = useContext(MenuContext);
+	const [data] = useState(Data);
 
-  const allCategories = ["all", ...new Set(data.map((item) => item.category))];
+	const allCategories = ["All", ...new Set(data.map((item) => item.category))];
 
-  const menuFilterHandler = (category) => {
-    if (category === "all") {
-      setMenu(data);
-      return;
-    }
-    const newItem = data.filter((newVal) => {
-      return newVal.category === category;
-    });
-    setMenu(newItem);
-  };
-  return (
-    <nav className={classes.navbar}>
-      <ul className={classes.options}>
-        {allCategories.map((category, index) => (
-          <li key={index} onClick={() => menuFilterHandler(category)}>
-            {category}
-          </li>
-        ))}
-      </ul>
-    </nav>
-  );
+	const menuFilterHandler = (category) => {
+		if (category === "All") {
+			setMenu(data);
+			return;
+		}
+		const newItem = data.filter((newVal) => {
+			return newVal.category === category;
+		});
+		setMenu(newItem);
+	};
+	return (
+		<nav className={classes.navbar}>
+			<ul className={classes.options}>
+				{allCategories.map((category, index) => (
+					<li key={index} onClick={() => menuFilterHandler(category)}>
+						{category}
+					</li>
+				))}
+			</ul>
+		</nav>
+	);
 };
 
 export default Navbar;
