cab26dd54704e3e527f6a79dede021138bc0265c
Ester Cortes <002escor@gmail.com>
2023-01-11 09:51:13 +0100

    style: fixed styling for cart component

--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -1,77 +1,73 @@
 import React, { useContext } from "react";
 import { Stack } from "react-bootstrap";
 import { CartContext } from "../data/CartContext";
-import classes from "./cart-wrapper.module.css";
+import classes from "./cart.module.css";
 import { CgTrashEmpty } from "react-icons/cg";
 const CartItem = ({ image, title, price, qty, id }) => {
-  const { dispatch } = useContext(CartContext);
+	const { dispatch } = useContext(CartContext);
 
-  return (
-    <>
-    <Stack direction="horizontal" gap={2}>
-    <div className={classes.header}>
+	return (
+		<>
+			<Stack direction="horizontal" gap={2}>
+				<div className={classes.header}>
+					<img
+						src={image}
+						alt={title}
+						style={{
+							width: "65px",
+							height: "30px",
+							borderRadius: "30px",
+							perspective: "1000px",
+						}}
+					/>
 
-      <img
-        src={image}
-        alt={title}
-        style={{
-          width: "65px",
-          height: "30px",
-          borderRadius: "30px",
-          perspective: "1000px",
-        }}
-      />
-
-        <div>
-          <span className={classes.title}>
-          {title}
-          </span>
-          <span className={classes.qty}>
-            <input
-              type="number"
-              as="select"
-              min={1}
-              max={5}
-              value={qty}
-              onChange={(e) =>
-                dispatch({
-                  type: "CHANGE_CART_QTY",
-                  payload: {
-                    id: id,
-                    qty: e.target.value,
-                  },
-                })
-              }
-            />
-          </span>
-          <div className="text-muted">${price.toFixed(2)} </div>
-        </div>
-      </div>
-        <div className={classes["single-item-total"]}>
-          ${+(price * qty).toFixed(2)}{" "}
-        </div>
-       
-
-    </Stack>
-    <Stack>
-       <div className={classes.el}>
-
-<div className={classes["delete-icon"]} onClick={()=>dispatch({
-  type:'REMOVE_FROM_CART',
-  payload: {
-    id: id,
-    
-  },
-  
-  })}>
-  <CgTrashEmpty  />
-</div>
-<input type="text" placeholder="Order Note..." />
-
-</div>
-    </Stack>
-    </>
-  );
+					<div>
+						<span className={classes.title}>{title}</span>
+						<span className={classes.qty}>
+							<input
+								type="number"
+								as="select"
+								min={1}
+								max={5}
+								value={qty}
+								onChange={(e) =>
+									dispatch({
+										type: "CHANGE_CART_QTY",
+										payload: {
+											id: id,
+											qty: e.target.value,
+										},
+									})
+								}
+							/>
+						</span>
+						<div className="text-muted">${price.toFixed(2)} </div>
+					</div>
+				</div>
+				<div className={classes["single-item-total"]}>
+					${+(price * qty).toFixed(2)}{" "}
+				</div>
+			</Stack>
+			<Stack>
+				<div className={classes.el}>
+					<div
+						className={classes["delete-icon"]}
+						onClick={() =>
+							dispatch({
+								type: "REMOVE_FROM_CART",
+								payload: {
+									id: id,
+								},
+							})
+						}
+					>
+						<CgTrashEmpty />
+					</div>
+					<input type="text" placeholder="Order Note..." />
+				</div>
+			</Stack>
+		</>
+	);
 };
 
 export default CartItem;
--- src/Components/Menu/CartMenu.js
+++ src/Components/Menu/CartMenu.js
@@ -1,53 +1,58 @@
-import React, { useContext,useState } from 'react'
+import React, { useContext, useState } from "react";
 import Dropdown from "react-bootstrap/Dropdown";
+import classes from "./menuContainer.module.css";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { Badge, Button } from "react-bootstrap";
-import { CartContext } from '../data/CartContext';
+import { CartContext } from "../data/CartContext";
 
-const CartMenu = () => {
-    const {state:{cart},dispatch,setShow} = useContext(CartContext);
+const CartMenu = (props) => {
+	const {
+		state: { cart },
+		dispatch,
+		setShow,
+	} = useContext(CartContext);
 
-    const handleShow = () => setShow(true);
+	const handleShow = () => setShow(true);
 
-    
+	return (
+		<Dropdown className={` ${props.className} d-inline mx-2 my-3 dropdown`}>
+			<Dropdown.Toggle id="dropdown-autoclose-true" variant="secondary">
+				<AiOutlineShoppingCart />
+				<Badge bg="secondary">{cart.length}</Badge>
+			</Dropdown.Toggle>
+			<Dropdown.Menu style={{ minWidth: 100, textAlign: "center" }}>
+				{cart.length > 0 ? (
+					<>
+						{cart.map((item) => (
+							<span key={item.id} className="cartitem">
+								<img
+									src={item.image}
+									alt={item.title}
+									className="cartItemImg"
+								/>
+								<div className="cartItemDetail">
+									<span>{item.title}</span>
+									<span>
+										{item.price.toFixed(2)} * {item.qty} qty
+									</span>
+								</div>
+							</span>
+						))}
+						<Dropdown.Item>
+							<Button
+								style={{ width: "95%", margin: "0 10px" }}
+								onClick={handleShow}
+							>
+								Go To Cart
+							</Button>
+						</Dropdown.Item>
+					</>
+				) : (
+					<span style={{ padding: 10 }}>Cart Is Empty</span>
+				)}
+			</Dropdown.Menu>
+		</Dropdown>
+	);
+};
 
-  return (
-    <Dropdown className="d-inline mx-2 my-3 dropdown">
-    <Dropdown.Toggle id="dropdown-autoclose-true" variant='secondary'>
-      <AiOutlineShoppingCart />
-      <Badge bg="secondary">{cart.length}</Badge>
-    </Dropdown.Toggle>
-    <Dropdown.Menu style={{ minWidth: 100, textAlign: "center" }}>
-      {cart.length > 0 ? (
-        <>
-          {cart.map((item) => (
-            <span key={item.id} className="cartitem">
-              <img
-                src={item.image}
-                alt={item.title}
-                className="cartItemImg"
-              />
-              <div className="cartItemDetail">
-                <span>{item.title}</span>
-                <span>
-                 
-                  {item.price.toFixed(2)} * {item.qty} qty
-                </span>
-              </div>
-            </span>
-          ))}
-          <Dropdown.Item >
-          <Button style={{ width: "95%", margin: "0 10px" }} onClick={handleShow}>
-            Go To Cart
-          </Button>
-          </Dropdown.Item>
-        </>
-      ) : (
-        <span style={{ padding: 10 }}>Cart Is Empty</span>
-      )}
-    </Dropdown.Menu>
-  </Dropdown>
-  )
-}
-
-export default CartMenu
+export default CartMenu;
